f018cf3dbe5f8b8ebf17a0877338092dbf3ec276
jtysorin <sorin.jitianu@gmail.com>
2023-08-10 08:47:20 +0300

    :added

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,7 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
-    core.notice('Hello from my custom JavaScript Action!');
+    core.notice('Deploy to AWS S3 bucket!');
 
     // 1) Get some input values
     const bucket = core.getInput('bucket', { required: true });
@@ -13,6 +13,9 @@ function run() {
     // 2) Upliad files
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
